1f6e6ea77f1259211330fc1aa14ba9fc8e879bcb
salokesh99 <salokesh99@gmail.com>
2022-06-24 20:03:29 +0530

    models draft added

--- hello/models.py
+++ hello/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 
-class Users(models.Model):
+class User(models.Model):
     username = models.CharField(unique=True, max_length=30)
     password = models.CharField(max_length=18)
     employee_id = models.CharField(unique=True, max_length=18)
@@ -25,55 +25,31 @@ class Users(models.Model):
     is_active = models.BooleanField(default=False)
 
 
-
-class Department(models.Model):
-
-    name = models.CharField(max_length=128)
-    email_id = models.EmailField(max_length=75)
-    mobile_number = models.CharField(max_length=13)
-    description = models.CharField(max_length=200)
-    #manager to be presented in Name and email format only.
-    manager = models.ForeignKey(Users)
-    members = models.ManyToManyField(
-        Users,
-        through='Membership',
-        through_fields=('group', 'person'),
-    )
-
-
-    # Regular Fields
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
-    is_archive = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-
-class Tickets(models.Model):
+class Ticket(models.Model):
 
     uuid = models.AutoField()
     manual_id = models.IntegerField(default=1)
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=2000)
-    # attachments = 
+    # attachments = models.FileField(upload_to ='uploads/')
     # status = 'optional_field'
     #  to be presented in Name and email format only
-    reported_by = models.ForeignKey(Users)
-    assigned_to = models.ForeignKey(Users)
-    reporter_department = models.ForeignKey(Department)
-    assignee_department = models.ForeignKey(Department)
+    reported_by = models.ForeignKey(User)
+    assigned_to = models.ForeignKey(User)
+    reporter_department = models.ForeignKey('Department')
+    assignee_department = models.ForeignKey('Department')
     # help_topics = 'to be decided from a pool of options of help topics 
     # tags = 'multiple individual words'
     # severity = 'to be chosen from severity options'
     # priority = 'to be chosen from priority options'
 
 
-class Attachment():
-
-
-
-
-
-
-
+class Attachment(models.Model):
+    name = models.CharField(max_length=128)
+    size = models.CharField(max_length=128)
+    file_type = models.CharField(max_length=128)
+    file_path = models.CharField(max_length=128)
+    is_edited = models.BooleanField(default=False)
 
     # Regular Fields
     created_at = models.DateTimeField(auto_now_add=True)
@@ -82,15 +58,28 @@ class Attachment():
     is_active = models.BooleanField(default=False)
 
 
+class Department(models.Model):
+    name = models.CharField(max_length=128)
+    size = models.CharField(max_length=128)
+    address = models.CharField(max_length=128)
+    contact_person = models.CharField(max_length=128)
+    contact_number = models.CharField(max_length=128)
+    email_id = models.EmailField(unique=True, max_length=75)
+    manager = models.ForeignKey(User)
+    manager_email_id = models.EmailField(unique=True, max_length=75)
+    Manager_contact = models.IntegerField()
+    members = models.ManyToManyField(
+        User,
+        through='Membership',
+        through_fields=('user', 'person'),
+    )
 
+    # Regular Fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_archive = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
-
-
-
-
-
-
-    # Rest of your model data
 
     def save(self, *args, **kwargs):
         # This means that the model isn't saved to the database yet
@@ -106,104 +95,218 @@ class Attachment():
         super(Tickets, self).save(*args, **kwargs)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True, max_length=255)
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    author = models.TextField()
-
-
 class Comment(models.Model):
-    name = models.CharField(max_length=42)
-    email = models.EmailField(max_length=75)
-    website = models.URLField(max_length=200, null=True, blank=True)
-    content = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    title = models.CharField(max_length=75)
+    description = models.TextField(max_length=2000)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
+    attachments = models.FileField(upload_to ='uploads/')
+    author = models.ForeignKey(User)
+
+    # Regular Fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_edited = models.BooleanField(default=False)
+    is_archive = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
 
-class Employee(models.Model):  
-    first_name = models.CharField(max_length=30)  
-    last_name = models.CharField(max_length=30) 
+class Templates(models.Model):
+    title = models.CharField(max_length=75)
+    description = models.TextField(max_length=2000)
+    department = models.ForeignKey('Department')
+    author = models.ForeignKey(User)
+    last_used_at = models.DateTimeField(auto_now_add=True)
 
-class Musician(models.Model):
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    instrument = models.CharField(max_length=200)
+    # Regular Fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_edited = models.BooleanField(default=False)
+    is_archive = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
-class Album(models.Model):
-    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    release_date = models.DateField()
-    num_stars = models.IntegerField()
+class HelpTopics(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    author = models.ForeignKey(User)
+
+    # Regular Fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_edited = models.BooleanField(default=False)
+    is_archive = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
 
 
-class Person(models.Model):
-    name = models.CharField(max_length=30)
-    def __str__(self):
-        return self.name
+class Shift(models.Model):
+    title = models.CharField(max_length=200)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    author = models.ForeignKey(User)
+
+    # Regular Fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_edited = models.BooleanField(default=False)
+    is_archive = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+class Roles(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    author = models.ForeignKey(User)
+
+    # Regular Fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_edited = models.BooleanField(default=False)
+    is_archive = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+
+class Priority(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    author = models.ForeignKey(User)
+
+    # Regular Fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_edited = models.BooleanField(default=False)
+    is_archive = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+
+class Severity(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    author = models.ForeignKey(User)
+
+    # Regular Fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_edited = models.BooleanField(default=False)
+    is_archive = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ++++++++++++++++++++++ draft ++++++++++++++++++++++++++++#
+# class Department(models.Model):
+
+#     name = models.CharField(max_length=128)
+#     email_id = models.EmailField(max_length=75)
+#     mobile_number = models.CharField(max_length=13)
+#     description = models.CharField(max_length=200)
+#     #manager to be presented in Name and email format only.
+#     manager = models.ForeignKey(Users)
+#     members = models.ManyToManyField(
+#         Users,
+#         through='Membership',
+#         through_fields=('group', 'person'),
+#     )
+
+#     # Regular Fields
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now_add=True)
+#     is_archive = models.BooleanField(default=False)
+#     is_active = models.BooleanField(default=False)
+
+    # Rest of your model data
+
+
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+
+
+# class Post(models.Model):
+#     title = models.CharField(max_length=255)
+#     slug = models.SlugField(unique=True, max_length=255)
+#     content = models.TextField()
+#     created_on = models.DateTimeField(auto_now_add=True)
+#     author = models.TextField()
+
+
+# class Comment(models.Model):
+#     name = models.CharField(max_length=42)
+#     email = models.EmailField(max_length=75)
+#     website = models.URLField(max_length=200, null=True, blank=True)
+#     content = models.TextField()
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     created_on = models.DateTimeField(auto_now_add=True)
+
+
+# class Employee(models.Model):  
+#     first_name = models.CharField(max_length=30)  
+#     last_name = models.CharField(max_length=30) 
+
+# class Musician(models.Model):
+#     first_name = models.CharField(max_length=200)
+#     last_name = models.CharField(max_length=200)
+#     instrument = models.CharField(max_length=200)
+
+# class Album(models.Model):
+#     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100)
+#     release_date = models.DateField()
+#     num_stars = models.IntegerField()
+
+
+# class Person(models.Model):
+#     name = models.CharField(max_length=30)
+#     def __str__(self):
+#         return self.name
         
-class Citizenship(models.Model):
-    person = models.OneToOneKey(Person, on_delete=models.CASCADE)
-    country = models.CharField(max_length=30)
+# class Citizenship(models.Model):
+#     person = models.OneToOneKey(Person, on_delete=models.CASCADE)
+#     country = models.CharField(max_length=30)
 
 
-class Customer(models.Model):
-    name = models.CharField(max_length=30)
-    def __str__(self):
-        return self.name
+# class Customer(models.Model):
+#     name = models.CharField(max_length=30)
+#     def __str__(self):
+#         return self.name
         
-class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    order_details = models.TextField()
+# class Order(models.Model):
+#     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+#     order_details = models.TextField()
 
 
-class Person(models.Model):
-    name = models.CharField(max_length=30)
+# class Person(models.Model):
+#     name = models.CharField(max_length=30)
 
-class Group(models.Model):
-    name = models.CharField(max_length=128)
-    members = models.ManyToManyField(
-        Person,
-        through='Membership',
-        through_fields=('group', 'person'),
-    )
+# class Group(models.Model):
+#     name = models.CharField(max_length=128)
+#     members = models.ManyToManyField(
+#         Person,
+#         through='Membership',
+#         through_fields=('group', 'person'),
+#     )
 
-class Membership(models.Model):
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    inviter = models.ForeignKey(
-        Person,
-        on_delete=models.CASCADE,
-        related_name="membership_invites",
-    )
-    invite_reason = models.CharField(max_length=64)
+# class Membership(models.Model):
+#     group = models.ForeignKey(Group, on_delete=models.CASCADE)
+#     person = models.ForeignKey(Person, on_delete=models.CASCADE)
+#     inviter = models.ForeignKey(
+#         Person,
+#         on_delete=models.CASCADE,
+#         related_name="membership_invites",
+#     )
+#     invite_reason = models.CharField(max_length=64)
     
